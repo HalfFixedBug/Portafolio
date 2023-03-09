a0df58a78e4214c4c6a2e8bb1d931c27c63ba3c2
HalfFixedBug <espinozaar.uno@gmail.com>
2023-03-09 16:33:37 -0300

    Mejoras de adaptabilidad
    Se agregaron enlaces a repositorios en cada card-image-container adaptado a dispositivos moviles y escritorio

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const navbar_toggler = document.querySelector(".navbar-toggler");
 const navbar_nav = document.querySelector(".navbar-nav");
 const navbar_collapse = document.querySelector(".navbar-collapse");
+const card_image_container = document.querySelectorAll(".card-image-container");
 navbar_toggler.addEventListener("click", ()=>{
     navbar_toggler.classList.toggle("active");
     navbar_nav.classList.toggle("active");
     navbar_collapse.classList.toggle("active");
 })
+
+card_image_container.forEach((e)=> e.addEventListener("click", ()=>{
+    e.classList.toggle("active");
+}))
